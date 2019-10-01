dae5158a436d34f3728445583a0131cd3184785c
Mike Lynch <mjlynch2@gmail.com>
2019-10-01 16:11:09 -0500

    add client side Axios get request to the /random url

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
   // Renders the entire app on the DOM
+  state = {
+    path: null,
+    description: ''
+  }
+
+
+  getRandomGif = () => {
+    Axios.get('/random')
+    .then((response) => {
+      this.setState({
+        path: response.path,
+        description: response.description
+      })
+    }).catch((error) => {
+      console.log('Error in GET:', error)
+    })
+  }
+
   render() {
     return (
       <div>
