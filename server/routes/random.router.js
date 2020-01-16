aba31551fad7abe4ea62977cf55dfb22e4943c32
Mike Lynch <mjlynch2@gmail.com>
2020-01-16 14:23:05 -0600

    Randomize the photo result page

--- server/routes/random.router.js
+++ server/routes/random.router.js
@@ -4,14 +4,14 @@ const axios = require('axios');
 require('dotenv').config();
 
 getRandomNumber = () => {
-    let random = (Math.floor(Math.random() * 100) + 1);
+    let random = (Math.floor(Math.random() * 75) + 1);
     console.log(random);
     return random;
 }
 
 router
   .get("/", (req, res) => {
-    let endpoint = `https://pixabay.com/api/?key=${process.env.API_KEY}&q=dogs&page=1&per_page-5`;
+    let endpoint = `https://pixabay.com/api/?key=${process.env.API_KEY}&q=dogs&page=${getRandomNumber()}&per_page-5`;
     axios
       .get(endpoint, {headers: {Authorization: process.env.API_KEY}})
       .then(response => {       
